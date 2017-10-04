2d4fa2fbe52d1311dd47bb15953905677d606f8d
Sujay Sanjay <mahadik203@gmail.com>
2017-10-04 23:36:04 +0530

    optimized and added comments to assignment 1

--- # 1/assignment1.sh
+++ # 1/assignment1.sh
@@ -1,53 +1,57 @@
 fileName="MyAddressBook"
 opt=1
 while [ "$opt" -lt 6 ]
-do 
+do
+
 	echo -e "Choose one of the Following\n1. Create a New Address Book\n2. View Records\n3. Insert new Record\n4. Delete a Record\n5. Modify a Record\n6. Exit"
+	# echo -e, enables special features of echo to use \n \t \b etc.
 	read opt
-	case $opt in 
+	case $opt in
 
 	1)
-		if [ -f $fileName ] ; then
+
+		if [ -e $fileName ] ; then	# -e to check if file exists, if exits remove the file
 			rm $fileName
 		fi
 		cont=1
+		echo -e "NAME\tNUMBER\t\tADDRESS\n===============================\n" | cat >> $fileName
 		while [ "$cont" -gt 0 ]
-		do 
-			echo "Enter Name"
+		do
+			echo -e "\nEnter Name"
 			read name
 			echo "Enter Phone Number of $name"
 			read number
 			echo "Enter Address of $name"
 			read address
-			echo -e "$name\t$number\t$address\n" | cat >> $fileName
-			echo "Enter 0 to Stop"
+			echo -e "$name\t$number\t\t$address" | cat >> $fileName
+			echo "Enter 0 to Stop, 1 to Enter next"
 			read cont
 		done
 		;;
 
-	2)	
+	2)
 		cat $fileName
 		;;
 	3)
-		echo "Enter Name"
+		echo -e "\nEnter Name"
 		read name
 		echo "Enter Phone Number of $name"
 		read number
 		echo "Enter Address of $name"
 		read address
-		echo -e "$name\t$number\t$address\n" | cat >> $fileName
+		echo -e "$name\t$number\t\t$address" | cat >> $fileName
 		;;
-	4) 
-		echo "Name/Phone Number"
+	4)
+		echo -e "Delete record\nEnter Name/Phone Number"
 		read pattern
 		temp="temp"
-		grep -v $pattern $fileName | cat >> $temp
+		grep -v $pattern $fileName | cat >> $temp # grep -v selects non-matching lines
 		rm $fileName
 		cat $temp | cat >> $fileName
 		rm $temp
 		;;
 	5)
-		echo "Name/Phone Number"
+		echo -e "Modify record\nEnter Name/Phone Number"
 		read pattern
 		temp="temp"
 		grep -v $pattern $fileName | cat >> $temp
@@ -60,7 +64,7 @@ do
 		read number
 		echo "Enter Address of $name"
 		read address
-		echo -e "$name\t$number\t$address\n" | cat >> $fileName
-		;;		
+		echo -e "$name\t$number\t$address" | cat >> $fileName
+		;;
 	esac
 done
