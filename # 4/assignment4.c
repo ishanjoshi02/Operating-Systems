6236e6af4f9d69ea3eda34b13ad2495129ac8598
Ishan JOshi <ishanjoshi02@gmail.com>
2017-11-07 09:28:22 +0530

    Changed Producer Consumer Problem

--- # 4/assignment4.c
+++ # 4/assignment4.c
@@ -1,162 +1,87 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <pthread.h>
-#include <semaphore.h>
-#include <unistd.h>
+#include<stdio.h>
+#include<pthread.h>
+#include<semaphore.h>
+#include<stdio.h>
+#include<unistd.h>
+void *producer(void * thread);
+void *consumer(void * thread);
+int count=0,in=0,out=0,a[5];
+sem_t full;
+sem_t empty;
+pthread_mutex_t mutex;
+int main()
+{
+	int i,j,p,c;
+	pthread_t pid[10],cid[10];
+	pthread_mutex_init(&mutex,0);
+	sem_init(&full,0,0);
+	sem_init(&empty,0,5);
 
-#define item_in 10
-#define item_out 100
-#define ctid_length 3
-
-int item_in_array[item_in], item_out_array[item_out];
-int item_in_count = 0, item_out_count = 0;
-int no = 0;
-
-pthread_t ptid, ctid[ctid_length], disp_tid;
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-sem_t full, empty;
-
-void swap(int *a, int *b) {
-
-  int temp = *a;
-  *a = *b;
-  *b = temp;
-
+printf("\nEnter no of producers:");
+scanf("%d",&p);
+printf("\nEnter no of consumers:");
+scanf("%d",&c);
+	for(i=0;i<p;i++)
+		pthread_create(&pid[i],NULL,(void*)producer,&i);
+	for(i=0;i<c;i++)
+		pthread_create(&cid[i],NULL,(void*)consumer,&i);
+	
+	
+	for(i=0;i<p;i++)
+	
+		pthread_join(pid[i],NULL);
+	for(i=0;i<c;i++)
+		pthread_join(cid[i],NULL);
+	sem_destroy(&full);
+	sem_destroy(&empty);
+	pthread_mutex_destroy(&mutex); 		
 }
 
-void sort() {
+void *producer(void * thread)
+{
+	int t=*(int *)thread;
+	while(1)
+	{
+	sem_wait(&empty);
+	pthread_mutex_lock(&mutex);
+	if(count>=5)
+	printf("\nBuffer is full");
+	else
+	{
+		a[in]=rand()%100;
+		printf("\nproducer %d produced:%d",t,a[in]);
 
-  int i, j;
+		in=(in+1)%5;
+		count++;
+	}
 
-  for (i = 0;i < no;i++) {
-
-    for (j = 0;j < (no - i);j++) {
-
-      if (item_out_array[j] > item_out_array[j + 1]) {
-
-        swap(&item_out_array[j], &item_out_array[j + 1]);
-
-      }
-
-    }
-
-  }
-
+	pthread_mutex_unlock(&mutex);
+	sem_post(&full);
+	sleep(1);
+	}
+        pthread_exit(0);
 }
 
-void *disp_output(void *var) {
+void *consumer(void *thread)
+{
+int t=*(int *)thread;
+	while(1)
+	{
+	sem_wait(&full);
+	pthread_mutex_lock(&mutex);
+	if(count<=0)
+	printf("\nBuffer is empty");
+	else
+	{
+		printf("\nconsumer %d consumed:%d",t,a[in]);
 
-  int i;
-  while (1) {
+		out=(out+1)%5;
+		count--;
+	}
 
-    sort();
-    for (i = 0;i < no;i++) {
-
-      printf("[%d]\n", item_out_array[i]);
-      sleep(3);
-
-    }
-
-  }
-
-}
-
-void add_item(int item) {
-
-  int i = 0;
-  if (no == 0) {
-
-    item_out_array[no] = item;
-
-  } else {
-
-    item_out_array[--i] = item;
-
-  }
-
-  no++;
-
-}
-
-void *producer(void *var) {
-
-  int item;
-
-  while (1) {
-
-    item = rand();
-    sem_wait(&empty);
-    pthread_mutex_lock(&mutex);
-
-    if (item_in_count < item_in) {
-
-      item_in_array[item_in_count] = item;
-      item_in_count++;
-
-    }
-
-    pthread_mutex_unlock(&mutex);
-    sem_post(&full);
-    sleep(1);
-
-  }
-
-  return NULL;
-
-}
-
-void *consumer(void *var) {
-
-  int item;
-
-  while (1) {
-
-    sem_wait(&full);
-    pthread_mutex_lock(&mutex);
-
-    if (item_in_count > 0) {
-
-      add_item(item_in_array[--item_in_count]);
-
-    }
-
-    pthread_mutex_unlock(&mutex);
-    sem_post(&empty);
-    sleep(1);
-
-  }
-
-  return NULL;
-
-}
-
-int main(int argc, char const *argv[]) {
-
-  int i;
-
-  sem_init(&empty, 0, 10);
-  sem_init(&full, 0, 0);
-
-  pthread_create(&ptid, NULL, producer, NULL);
-
-  for (i = 0;i < ctid_length;i++) {
-
-    pthread_create(&ctid[i], NULL, consumer, NULL);
-
-  }
-
-  pthread_create(&disp_tid, NULL, disp_output, NULL);
-
-  pthread_join(ptid, NULL);
-
-  for (i = 0; i < ctid_length;i++) {
-
-    pthread_join(ctid[i], NULL);
-
-  }
-
-  pthread_join(disp_tid, NULL);
-
-  return 0;
-
+	pthread_mutex_unlock(&mutex);
+	sem_post(&empty);
+	sleep(1);
+	}
+        pthread_exit(0);
 }
